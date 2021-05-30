df63aa78dfd2340323ece49db19823d858851053
konghiran.w <konghiran.w@linecorp.com>
2021-05-30 07:00:25 +0700

    update server to use multiplexer

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,23 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/",handler)
-	_ = http.ListenAndServe(":8082", nil)
+	StartServerMultiplexer()
 }
 
-func handler(writer http.ResponseWriter, request *http.Request)  {
+func index(writer http.ResponseWriter, request *http.Request) {
 	_, _ = fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+func StartServerMultiplexer()  {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/",files))
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr: "0.0.0.0:8082",
+		Handler: mux,
+	}
+	_ = server.ListenAndServe()
 }
